24ffd6cf65c04c0899a1ae84d2ef236710c7ef0e
honmanyau <honmanyau@users.noreply.github.com>
2021-08-21 20:03:56 +1000

    Database connection setup.

---
+++ database/index.ts
@@ -0,0 +1,30 @@
+import path from 'path';
+import { Sequelize } from 'sequelize';
+
+export const sequelize = new Sequelize({
+  dialect: 'sqlite',
+  storage:
+    process.env.NODE_ENV === 'production'
+      ? path.join(process.cwd(), '/database/markr.sqlite')
+      : ':memory:',
+  logging: process.env.NODE_ENV === 'production' ? false : console.log,
+});
+
+// ===============
+// == Functions ==
+// ===============
+/**
+ * This function exports an async function for testing the sequelize instance
+ * initialised above. It is intended to be use to ensure a database connection
+ * is available before starting the Node.js server.
+ */
+export async function init(): Promise<void> {
+  try {
+    await sequelize.authenticate();
+    await sequelize.sync();
+
+    console.log('Successfully established connection to the database.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+}
