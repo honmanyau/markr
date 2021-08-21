58d530c5a097f4a59e7b89dbceca90c49a98f829
honmanyau <honmanyau@users.noreply.github.com>
2021-08-21 19:39:42 +1000

    Basic endpoint setup.

--- src/index.ts
+++ src/index.ts
@@ -15,6 +15,35 @@ app.use(helmet());
 app.get('/', (_request, response) => {
   response.status(200).send({ ok: true });
 });
+app.post('/import', (request, response) => {
+  if (request.get('Content-Type') === 'text/xml+markr') {
+    // TODO: data parsing, validation, processing, and storage.
+    
+    response
+      .status(200)
+      .send({
+        ok: true
+      });
+  }
+  else {
+    response
+      .status(415)
+      .send({
+        ok: false,
+        statusCode: 415,
+        error: 'Unsupported media type.',
+        message: 'Please check the Content-Type header is correctly set.'
+      });
+  }
+});
+app.get('/results/:testId/aggregate', (request, response) => {
+  response
+    .status(200)
+    .send({
+      ok: true,
+      message: `Statistics for test ID ${request.params.testId}`
+    });
+});
 
 // ===========
 // == Start ==
