cc39b99acaf4607097ff0336d367d2cc1e799515
honmanyau <honmanyau@users.noreply.github.com>
2021-08-21 22:58:40 +1000

    Formatting.

--- tests/endpoints/import.spec.ts
+++ tests/endpoints/import.spec.ts
@@ -38,16 +38,18 @@ describe('POST /import', () => {
       supertest(app)
         .post('/import')
         .set('Content-Type', 'text/xml+markr')
-        .send([
-          '<mcq-test-results>',
-          ' <mcq-test-result scanned-on="2017-12-04T12:12:10+11:00">',
-          '   <first-name>KJ</first-name>',
-          '   <last-name>Alysander</last-name>',
-          '   <student-number>002299</student-number>',
-          '   <summary-marks available="20" obtained="13" />',
-          ' </mcq-test-result>',
-          '</mcq-test-results>',
-        ].join('\n'))
+        .send(
+          [
+            '<mcq-test-results>',
+            ' <mcq-test-result scanned-on="2017-12-04T12:12:10+11:00">',
+            '   <first-name>KJ</first-name>',
+            '   <last-name>Alysander</last-name>',
+            '   <student-number>002299</student-number>',
+            '   <summary-marks available="20" obtained="13" />',
+            ' </mcq-test-result>',
+            '</mcq-test-results>',
+          ].join('\n')
+        )
         .expect(202)
         .end((error, _res) => {
           if (error) {
@@ -59,13 +61,12 @@ describe('POST /import', () => {
     }
   );
 
-  it(
-    'should respond with a status code of 201 for normally processed documents',
-    (done) => {
-      supertest(app)
-        .post('/import')
-        .set('Content-Type', 'text/xml+markr')
-        .send([
+  it('should respond with a status code of 201 for normally processed documents', (done) => {
+    supertest(app)
+      .post('/import')
+      .set('Content-Type', 'text/xml+markr')
+      .send(
+        [
           '<mcq-test-results>',
           ' <mcq-test-result scanned-on="2017-12-04T12:12:10+11:00">',
           '   <first-name>KJ</first-name>',
@@ -75,15 +76,15 @@ describe('POST /import', () => {
           '   <summary-marks available="20" obtained="13" />',
           ' </mcq-test-result>',
           '</mcq-test-results>',
-        ].join('\n'))
-        .expect(201)
-        .end((error, _res) => {
-          if (error) {
-            return done(error);
-          }
+        ].join('\n')
+      )
+      .expect(201)
+      .end((error, _res) => {
+        if (error) {
+          return done(error);
+        }
 
-          return done();
-        });
-    }
-  );
+        return done();
+      });
+  });
 });
