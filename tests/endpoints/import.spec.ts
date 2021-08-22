0f24d793ecf3caf4512c3d3739aa510462cd47df
honmanyau <honmanyau@users.noreply.github.com>
2021-08-22 20:37:36 +1000

    Added more tests with the help of new utilities.

--- tests/endpoints/import.spec.ts
+++ tests/endpoints/import.spec.ts
@@ -2,8 +2,39 @@ import { Server } from 'http';
 import supertest from 'supertest';
 import { TEST_SERVER_PORT } from '../../markr.config';
 import { app, database } from '../../src';
+import { createDocument } from '../utils';
 
 describe('POST /import', () => {
+  const entry1 = {
+    scannedOn: new Date('2017-12-04T12:12:10+11:00'),
+    firstName: 'KJ',
+    lastName: 'Alysander',
+    studentNumber: '002299',
+    testId: '9863',
+    availableMarks: 20,
+    obtainedMarks: 13,
+  };
+
+  const entry2 = {
+    scannedOn: new Date('2017-12-04T12:14:11+11:00'),
+    firstName: 'Nyan',
+    lastName: 'Pasu',
+    studentNumber: '1007',
+    testId: '9863',
+    availableMarks: 20,
+    obtainedMarks: 19,
+  };
+
+  const entry3 = {
+    scannedOn: new Date('2017-12-04T12:14:11+11:00'),
+    firstName: 'Alice',
+    lastName: 'Bob',
+    studentNumber: '4224',
+    testId: '9863',
+    availableMarks: 20,
+    obtainedMarks: 17,
+  };
+  
   let server: Server;
 
   beforeAll(async () => {
@@ -76,30 +107,89 @@ describe('POST /import', () => {
     }
   );
 
-  it('should respond with a status code of 201 for normally processed documents', (done) => {
-    supertest(app)
-      .post('/import')
-      .set('Content-Type', 'text/xml+markr')
-      .send(
-        [
-          '<mcq-test-results>',
-          ' <mcq-test-result scanned-on="2017-12-04T12:12:10+11:00">',
-          '   <first-name>KJ</first-name>',
-          '   <last-name>Alysander</last-name>',
-          '   <student-number>002299</student-number>',
-          '   <test-id>9863</test-id>',
-          '   <summary-marks available="20" obtained="13" />',
-          ' </mcq-test-result>',
-          '</mcq-test-results>',
-        ].join('\n')
-      )
-      .expect(201)
-      .end((error, _res) => {
-        if (error) {
-          return done(error);
-        }
+  it(
+    'should respond with a status code of 201 for normally processed documents'
+    + ' containing a single valid entry',
+    (done) => {
+      supertest(app)
+        .post('/import')
+        .set('Content-Type', 'text/xml+markr')
+        .send(createDocument([
+          entry1
+        ]))
+        .expect(201)
+        .end((error, _res) => {
+          if (error) {
+            return done(error);
+          }
 
-        return done();
-      });
-  });
+          return done();
+        });
+    });
+
+  it(
+    'should respond with a status code of 201 for normally processed documents'
+    + ' containing 2 valid entries',
+    (done) => {
+      supertest(app)
+        .post('/import')
+        .set('Content-Type', 'text/xml+markr')
+        .send(createDocument([
+          entry1,
+          entry2,
+        ]))
+        .expect(201)
+        .end((error, _res) => {
+          if (error) {
+            return done(error);
+          }
+
+          return done();
+        });
+    });
+
+  it(
+    'should respond with a status code of 201 for normally processed documents'
+    + ' containing 3 entries',
+    (done) => {
+      supertest(app)
+        .post('/import')
+        .set('Content-Type', 'text/xml+markr')
+        .send(createDocument([
+          entry1,
+          entry2,
+          entry3,
+        ]))
+        .expect(201)
+        .end((error, _res) => {
+          if (error) {
+            return done(error);
+          }
+
+          return done();
+        });
+    });
+
+  it(
+    'should respond with a status code of 201 for normally processed documents'
+    + ' containing 4 entries, one of which is duplicated',
+    (done) => {
+      supertest(app)
+        .post('/import')
+        .set('Content-Type', 'text/xml+markr')
+        .send(createDocument([
+          entry1,
+          entry2,
+          entry3,
+          entry1,
+        ]))
+        .expect(201)
+        .end((error, _res) => {
+          if (error) {
+            return done(error);
+          }
+
+          return done();
+        });
+    });
 });
