af025545ea9eb2fb2d17a5bfc28f54f2541b7355
honmanyau <honmanyau@users.noreply.github.com>
2021-08-22 11:23:09 +1000

    Added `UPLOAD_LIMIT` constant.

--- markr.config.ts
+++ markr.config.ts
@@ -2,4 +2,5 @@ import path from 'path';
 
 export const SERVER_PORT = 4567;
 export const TEST_SERVER_PORT = 7654;
+export const UPLOAD_LIMIT = '256mb';
 export const DB_PATH = path.join(process.cwd(), '/database/markr.sqlite');
