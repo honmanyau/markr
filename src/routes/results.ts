23eedab7a5e93491b842dcbc76ee50ce9046c3d9
honmanyau <honmanyau@users.noreply.github.com>
2021-08-22 11:55:58 +1000

    Implemented basic logic for handling requests at the `/:testId/aggregate` endpoint.

--- src/routes/results.ts
+++ src/routes/results.ts
@@ -1,12 +1,29 @@
 import { Router } from 'express';
+import { Result } from '../../database/models';
 
 const router = Router();
 
-router.get('/:testId/aggregate', (request, response) => {
-  response.status(200).send({
-    ok: true,
-    message: `Statistics for test ID ${request.params.testId}`,
+router.get('/:testId/aggregate', async (request, response) => {
+  const testId = request.params.testId;
+  const results = await Result.findAll({
+    where: { testId }
   });
+
+  if (results.length > 0) {
+    // TODO: handle statistics.
+    response.status(200).send({
+      ok: true,
+      message: `Statistics for test ID ${testId}`,
+    });
+  }
+  else {
+    response.status(404).send({
+      ok: false,
+      statusCode: 404,
+      error: 'Not found.',
+      message: `No record with test ID ${testId} was found.`,
+    });
+  }
 });
 
 export default router;
