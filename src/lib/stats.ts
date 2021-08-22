886782a29a6af29b17d92f78a1dc86e3f3906aa3
honmanyau <honmanyau@users.noreply.github.com>
2021-08-22 13:57:39 +1000

    Added stubs for population variance and SD. Commented out sample SD.

--- src/lib/stats.ts
+++ src/lib/stats.ts
@@ -25,14 +25,53 @@ export function nearestRankPercentile(nums: number[], n: number): number {
   return sum;
 }
 
+// /**
+//  * This function calculates the sample standard deviation of the given array.
+//  * 
+//  * @param {number[]} nums An array of numbers.
+//  * @param {number} n The precentile to calculate.
+//  * @returns {number} The n-th percentile of the input array.
+//  */
+//  export function sampleStddev(nums: number[]): number {
+//   let sum = -Infinity;
+
+//   return sum;
+// }
+
 /**
- * This function calculates the sample standard deviation of the given array.
+ * This function calculates the sum of the given array.
+ * 
+ * @param {number[]} nums An array of numbers.
+ * @returns {number} The sum of all numbers in the input array.
+ */
+ export function sum(nums: number[]): number {
+  let sum = -Infinity;
+
+  return sum;
+}
+
+/**
+ * This function calculates the population standard deviation of the given
+ * array.
  * 
  * @param {number[]} nums An array of numbers.
  * @param {number} n The precentile to calculate.
- * @returns {number} The n-th percentile of the input array.
+ * @returns {number} The population standard deviation of the input array.
  */
- export function sampleStddev(nums: number[]): number {
+ export function populationStddev(nums: number[]): number {
+  let sum = -Infinity;
+
+  return sum;
+}
+
+/**
+ * This function calculates the population variance of the given array.
+ *
+ * @param {number[]} nums An array of numbers.
+ * @param {number} n The precentile to calculate.
+ * @returns {number} The population variance of the input array.
+ */
+ export function populationVariance(nums: number[]): number {
   let sum = -Infinity;
 
   return sum;
