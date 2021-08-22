1d941d1a6d603281d263d09081052b5527fbe673
honmanyau <honmanyau@users.noreply.github.com>
2021-08-22 12:53:52 +1000

    Added stubs for stats functions.

---
+++ src/lib/stats.ts
@@ -0,0 +1,39 @@
+/**
+ * This function calculates and returns the mean of the numbers in an array.
+ * 
+ * @param {number[]} nums An array of numbers.
+ * @returns {number} The mean of the values in the input array.
+ */
+export function mean(nums: number[]): number {
+  let sum = -Infinity;
+
+  return sum;
+}
+
+/**
+ * This function calculates the n-th percentile of the given array using
+ * the {@link https://en.wikipedia.org/wiki/95th_percentile | nearest-rank}
+ * method.
+ * 
+ * @param {number[]} nums An array of numbers.
+ * @param {number} n The precentile to calculate.
+ * @returns {number} The n-th percentile of the input array.
+ */
+export function nearestRankPercentile(nums: number[], n: number): number {
+  let sum = -Infinity;
+
+  return sum;
+}
+
+/**
+ * This function calculates the sample standard deviation of the given array.
+ * 
+ * @param {number[]} nums An array of numbers.
+ * @param {number} n The precentile to calculate.
+ * @returns {number} The n-th percentile of the input array.
+ */
+ export function sampleStddev(nums: number[]): number {
+  let sum = -Infinity;
+
+  return sum;
+}
