09697a4f56f3eda7ffd0dd747dab034b07076ff1
honmanyau <honmanyau@users.noreply.github.com>
2021-08-23 23:05:44 +1000

    Fixed previously uncommitted delete.

--- markr.config.ts
+++
@@ -1,6 +0,0 @@
-import path from 'path';
-
-export const SERVER_PORT = 4567;
-export const TEST_SERVER_PORT = 7654;
-export const UPLOAD_LIMIT = '256mb';
-export const PRODUCTION_DB = path.join(process.cwd(), '/database/markr.sqlite');
