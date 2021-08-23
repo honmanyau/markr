b7f3049145d41553d328e881b7e691180026b872
honmanyau <honmanyau@users.noreply.github.com>
2021-08-23 23:20:42 +1000

    Refactored.

--- src/routes/results.ts
+++ src/routes/results.ts
@@ -1,5 +1,9 @@
 import { Router } from 'express';
 import { Result } from '../database/models';
+import {
+  default as Statistics,
+  StatisticsAttributes
+} from '../database/models/Statistics';
 import * as stats from '../lib/stats';
 
 const router = Router();
@@ -11,29 +15,11 @@ router.get('/:testId/aggregate', async (request, response) => {
   });
 
   if (results.length > 0) {
-    const percentageMarks = results
-      .map((v) => v.percentageMark)
-      .sort((a, b) => a - b);
-
-    const mean = stats.mean(percentageMarks);
-    const count = percentageMarks.length;
-    const p25 = stats.nearestRankPercentile(percentageMarks, 0.25);
-    const p50 = stats.nearestRankPercentile(percentageMarks, 0.5);
-    const p75 = stats.nearestRankPercentile(percentageMarks, 0.75);
-    const min = Math.min(...percentageMarks);
-    const max = Math.max(...percentageMarks);
-    const stddev = stats.populationStddev(percentageMarks);
+    const summary = summariseResults(results);
 
     response.status(200).send({
       testId,
-      mean,
-      count,
-      p25,
-      p50,
-      p75,
-      min,
-      max,
-      stddev,
+      ...summary
     });
   } else {
     response.status(404).send({
@@ -44,5 +30,40 @@ router.get('/:testId/aggregate', async (request, response) => {
     });
   }
 });
+
+/**
+ * This function create a statistics summary of an array of Result records
+ * with the same test ID.
+ * 
+ * @param {Results[]} results An array of `Result`s.
+ * @returns {StatisticsAttributes} Statistics summary of the records provided.
+ */
+function summariseResults(
+  records: Result[]
+): Omit<StatisticsAttributes, 'testId'> {
+  const percentageMarks = records
+    .map((v) => v.percentageMark)
+    .sort((a, b) => a - b);
+
+  const mean = stats.mean(percentageMarks);
+  const count = percentageMarks.length;
+  const p25 = stats.nearestRankPercentile(percentageMarks, 0.25);
+  const p50 = stats.nearestRankPercentile(percentageMarks, 0.5);
+  const p75 = stats.nearestRankPercentile(percentageMarks, 0.75);
+  const min = Math.min(...percentageMarks);
+  const max = Math.max(...percentageMarks);
+  const stddev = stats.populationStddev(percentageMarks);
+  
+  return {
+    mean: stats.mean(percentageMarks),
+    count: percentageMarks.length,
+    p25: stats.nearestRankPercentile(percentageMarks, 0.25),
+    p50: stats.nearestRankPercentile(percentageMarks, 0.5),
+    p75: stats.nearestRankPercentile(percentageMarks, 0.75),
+    min: Math.min(...percentageMarks),
+    max: Math.max(...percentageMarks),
+    stddev: stats.populationStddev(percentageMarks),
+  };
+}
 
 export default router;
